9dbc8873a2124c7d9c634994c8401f06fbe38b44
Leonard Woody <leonard.woody@gmail.com>
2019-12-24 22:38:37 -0500

    new files

---
+++ Group.py
@@ -0,0 +1,7 @@
+class Group:
+    def __init__(self):
+        self._G = {0 + 0j, 1}
+
+    def Op(self, a, b):
+        return a * b
+        
---
+++ __main__.py
@@ -0,0 +1,13 @@
+from Group import Group
+
+def main():
+    print("Hello World")
+    g = Group()
+    a = g.Op(2+1j,2+5j)
+    print (a)
+
+
+
+
+if __name__ == "__main__":
+    main()
