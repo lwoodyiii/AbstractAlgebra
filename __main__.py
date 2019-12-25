f695776f6ad3aab1825a5341dba87bcca35c13e9
Leonard Woody <leonard.woody@gmail.com>
2019-12-24 23:19:54 -0500

    add tests

--- __main__.py
+++ __main__.py
@@ -4,14 +4,14 @@ from Group import Group
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("basic logging")
-    
     g = Group()
     a = g.Op(2+1j,2+5j)
     print (a)
 
-
-
-
+def test_main():
+    g = Group()
+    a = g.Op(1,1)
+    assert a == 1
 
 if __name__ == "__main__":
     main()
