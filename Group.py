c868cf7a37b2f8d9a2872799a4df1b35271f7e77
Leonard Woody <leonard.woody@gmail.com>
2019-12-27 17:36:15 -0500

    Group:

--- Group.py
+++ Group.py
@@ -3,8 +3,6 @@ from Set import Set
 class Group(Set):
     def __init__(self):
         super().__init__()
-        self.identity
+        self.identity = 1
 
     def inv(self, x): pass
-        
-    
